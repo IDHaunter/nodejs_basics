3da4b4f1d4d7be36b6add8f2bc6a789298db4a61
S0025774733 <d.salavei@skybuffer.com>
2023-11-20 20:43:12 +0100

    feat: express and ejs

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-const utils = require('./core/utils');
-const fs = require('fs');
-const http = require('http');
-
-console.log(`${utils.now()} - index.js ***********`);
-
-let server = http.createServer((req, res) => {
-    //res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'}); //text/plain;
-    //res.end('Testing <b>Node JS</b> features');
-
-    res.setHeader('My-special-header', 'nodeJs');
-
-	switch (req.url) {
-        case '/':
-            res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-            fs.createReadStream('./templates/index.html').pipe(res);
-          break;
-        case '/about':
-            res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-            fs.createReadStream('./templates/about.html').pipe(res);
-          break;
-        default:
-            res.writeHead(404, {'Content-Type':'text/html; charset=utf-8'});
-            fs.createReadStream('./templates/error.html').pipe(res);
-            //res.end('Error 404. Page not found');
-      }
-
-    //const stream = fs.createReadStream('./templates/index.html').pipe(res);
-    //stream.pipe(res);
-} )
-
+const express = require('express');
 const PORT = 3000;
-const HOST = 'localhost'; //127.0.0.1
 
-server.listen(PORT, HOST, ()=>{
-console.log(`Server is working: http://${HOST}:${PORT}`)
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    //res.sendFile(__dirname +'/templates_express/index.html');
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    //res.send('About us');
+    res.render('about');
+})
+
+app.get('/user/:username/:id', (req, res) => {
+    //res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`);
+    let data = 
+    {
+    username: req.params.username,
+    id: req.params.id,
+    hobbies: ['Football','Skate','Basketball']
+    };
+    res.render('user',data);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`);
 });
